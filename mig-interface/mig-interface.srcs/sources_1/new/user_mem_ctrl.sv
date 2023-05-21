4c3c471f9b2210053fe2df60bf2ae3782d6c65c1
yyick <yongyickw@gmail.com>
2023-05-21 17:59:57 +1000

    adjusted transaction data width with the ddr2 memory

--- mig-interface/mig-interface.srcs/sources_1/new/user_mem_ctrl.sv
+++ mig-interface/mig-interface.srcs/sources_1/new/user_mem_ctrl.sv
@@ -23,7 +23,7 @@
 /*----------------------------------------
 CONSTRUCTION + BACKGROUND
 -------------
-Setup:
+MIG Setup:
 1. PHY to controller clock ratio: 2:1
 2. data width: 16-bit;
 3. write and read data width: 16-bit;
@@ -36,38 +36,59 @@ Note:
 3. wr_data_mask This bus is the byte enable (data mask) for the data currently being written to the external memory. The byte to the memory is written when the corresponding wr_data_mask signal is deasserted. 
 
 General Construction:
-1. this is cross-domain clock by nature;
-2. MIG interface has its own clock to drive the read and write operation;
+1. this is cross-domain clock because 
+2. the MIG UI provides its own clock to drive the MIG interface with DDR2;
+2.1 the MIG interface has its own clock to drive the read and write operation;
 3. we use synchronizers to handle the CDC;
 4. however, only the control signals shall be synchronized;
 5. by above, address and data must remain stable from the user request to the complete flag;
 6. this is usually the case since we assume sequential transfer;
 
-Write Construction:
-1. By above, when writing, two clock cycles are required;
-2. since we only concern with 16-bit; we only need to deal with the first 64-bit chunk;
-3. we could ignore the seoond 64-bit chunk by masking it off;
-4. Also, one need to push the data to the MIG write FIFO before submitting the write request; 
+CDC:
+1. by above, we need to becareful;
+2. ??
 
-Write Address:
-1. ??
+Write Construction:
+1. By above, when writing, two clock cycles are required to complete the entire 128-bit data;
+2. one 64-bit batch per clock cycle;
+3. depending on the application, masking is required to mask out those bytes that are not required to be written;
+4. by above, the user needs to explicitly assert a data end flag to signal to the DDR2 for the second batch data;
+4. Also, one need to push the data to the MIG write FIFO before submitting the write request; otherwise, 
+    the write operation will not match the expectation (this is observed after some experimentation); 
 
 Read Construction:
-1. similar t th write operation, it takes two cycles to read all 128-bit data;
+1. similar to th write operation, it takes two cycles to read all 128-bit data;
 2. MIG will signal when the data is valid, and when the data is the last chunk on the data bus;
-3. since we are only dealing with 16-bit, ???
 
-Read Address:
-1. ??
+Background + Address Mapping;
+1. MIG is configured to map the DDR2 as rank-bank-row-column;
+2. see the data sheet, the address is 27-bit wide (including the rank); 
+2. since there is only one rank, this is hard-coded as zero; not important;
+3. DDR2 native data width i 16-bit; that means each address bit represents 16-bit; (see the datasheet);
+4. by above, each read/write DDR2 transaction is 128-bit;
+5. this corresponds to 128/16 = 8 address bits;
+6. by above, it implies that the three LSB bits of the address must be zero (the first three LSB column bits);
+7. This is because 3-bit corresponds to eight (8) 16-bit data for the DDR2;
+
+User Setup:
+1. by above, we have the write and read data to be 128-bit wide;
+2. we could always do the masking outside of the mig; so not critical;
+3. by above, the user address shall be 23-bit wide (27-1-3 = 23) where -1 is for the rank; -3 is for the column as discussed above;
+
+Application Setup:
+1. by above, 128-bit transaction is in place as not to waste the space;
+2. as such, the application needs to adjust with the setup;
+3. for example, if the application write data is only 16-bit, the application
+needs to accumulate 8 data before writing it;
+
 ----------------------------------------*/
 
 module user_mem_ctrl
     #(parameter
-        CLOCK_RATIO = 2,            // PHY to controller clock ratio;
-        DATA_WIDTH = 16,            // ddr2 native data width;
-        TRANSACTION_WIDTH = 64,     // per clock; so 128 in two clocks;
-        DATA_MASK_WIDTH = 8         // masking for write data; see UG586 for the formulae;
-        
+        CLOCK_RATIO                 = 2,            // PHY to controller clock ratio;
+        DATA_WIDTH                  = 128,            // ddr2 native data width;
+        TRANSACTION_WIDTH_PER_CYCLE = 64,     // per clock; so 128 in two clocks;
+        DATA_MASK_WIDTH             = 8         // masking for write data; see UG586 for the formulae;        
     )
     (
         /* -----------------------------------------------------
@@ -86,8 +107,7 @@ module user_mem_ctrl
         
         // data;
         input logic [DATA_WIDTH-1:0] user_wr_data,   
-        //output logic [DATA_WIDTH-1:0] user_rd_data,
-        output logic [63:0] user_rd_data,   // temporary;
+        output logic [DATA_WIDTH-1:0] user_rd_data,         
         
         // status
         output logic MIG_user_init_complete,        // MIG done calibarating and initializing the DDR2;
@@ -161,11 +181,11 @@ module user_mem_ctrl
     logic [2:0] app_cmd;  // input [2:0]                                  app_cmd
     logic app_en;  // input                                        app_en
     
-    logic [TRANSACTION_WIDTH-1:0] app_wdf_data;  // input [63:0]    app_wdf_data
+    logic [TRANSACTION_WIDTH_PER_CYCLE-1:0] app_wdf_data;  // input [63:0]    app_wdf_data
     logic app_wdf_end;  // input                                        app_wdf_end
     logic app_wdf_wren;  // input                                        app_wdf_wren
     
-    logic [TRANSACTION_WIDTH-1:0] app_rd_data;  // output [63:0]   app_rd_data
+    logic [TRANSACTION_WIDTH_PER_CYCLE-1:0] app_rd_data;  // output [63:0]   app_rd_data
     logic app_rd_data_end;  // output                                       app_rd_data_end
     logic app_rd_data_valid;  // output                                       app_rd_data_valid
     logic app_rdy;  // output                                       app_rdy
@@ -382,7 +402,8 @@ module user_mem_ctrl
         
         user_rd_data    = 0;
         //user_rd_data = app_rd_data;
-        
+        app_wdf_data    = 64'b0;  
+                
         /* -----------------------------------------------
         * state;
         * ST_WAIT_INIT_COMPLETE: to check MIG initialization complete status before doing everything else;
@@ -440,9 +461,9 @@ module user_mem_ctrl
                     //app_wdf_mask = 8'b1111_1100;
                     //app_wdf_data = {48'h0, user_wr_data};
                     
-                    // test write data;
+                    // mask the data out since this batch is not required;
                     app_wdf_mask = 8'hFF;
-                    app_wdf_data = {16'h4444, 16'h3333, 16'h2222, 16'h1111};
+                    app_wdf_data = 64'b0;   // dont care;
                     
                     // push it to the MIG write fifo;
                     app_wdf_wren = 1'b1;    
@@ -486,7 +507,8 @@ module user_mem_ctrl
                 
                     // test write data;
                     app_wdf_mask = 8'h00;
-                    app_wdf_data = {16'hFFFF, 16'hEEEE, 16'hDDDD, 16'hCCCC};
+                    //app_wdf_data = {16'hFFFF, 16'hEEEE, 16'hDDDD, 16'hCCCC};  // dummy data;
+                    app_wdf_data = user_wr_data;
                     
                     // indicate that the data on the app_wdf_data[] bus in the current cycle is the last 
                     // data for the current request.
