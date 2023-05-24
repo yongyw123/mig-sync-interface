200f2a4c97fc08cec7dc2cd55ea700ef5f9427c7
yyick <yongyickw@gmail.com>
2023-05-24 13:34:35 +1000

    updated tb to simulate MIG reset

--- mig-interface/mig-interface.srcs/sim_1/new/test_top_tb.sv
+++ mig-interface/mig-interface.srcs/sim_1/new/test_top_tb.sv
@@ -23,7 +23,8 @@
 module test_top_tb
     (
         input logic clk_in_100M,
-        input logic [15:0] LED               
+        input logic [15:0] LED,
+        output logic CPU_RESETN            
     );
     
     
@@ -41,6 +42,29 @@ module test_top_tb
         wait(LED[13:0] == 0);
         
         @(posedge clk_in_100M);
+        
+        
+        // reset yp start over;
+        CPU_RESETN = 1'b0;
+        #(100);
+        CPU_RESETN = 1'b1;
+        #(100);
+        
+        
+        // wait for the LED to increase;
+        // and wraps around twice to conclude the simulation;    
+        wait(LED[13:0] == 1);
+        
+        // first round is done;
+        wait(LED[13:0] == 0);
+        wait(LED[13:0] == 1);
+        
+        // second round is done;
+        wait(LED[13:0] == 0);
+        
+        @(posedge clk_in_100M);
+        
+        
         $stop; 
     end
 endmodule
